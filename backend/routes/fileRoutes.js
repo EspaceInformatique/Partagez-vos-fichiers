e87aa7ab90088efe8fb4feb7dac589229206ef6f
EspaceInformatique <eleveposte001@gmail.com>
2025-02-16 18:26:47 +0100

    Update fileRoutes.js

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 // Stockage temporaire des fichiers (en mémoire)
