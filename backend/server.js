8d44d5fa0623a1b8ffd97848ffca35a9ce291b00
EspaceInformatique <eleveposte001@gmail.com>
2025-02-16 18:26:21 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const fileRoutes = require('./routes/fileRoutes');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const PORT = 3000;
 
-// Middleware pour parser les requêtes JSON
+// Middleware pour parser les requêtes JSON et les fichiers
 app.use(express.json());
+app.use(fileUpload());
 
 // Servir les fichiers statiques (frontend)
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../')));
 
 // Routes pour les fichiers
 app.use('/api/files', fileRoutes);
